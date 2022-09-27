196074c49bcd165a4bf6a010d9562ee9693e7ab8
matebe12 <matebe12@gmail.com>
2022-09-27 23:24:44 +0900

    1week practice code

--- src/drag.js
+++ src/drag.js
@@ -39,80 +39,26 @@ const Drag = ()  => {
     //드래그 시작
     const onDragStart = (event) => {
         console.log('드래그 시작')
-        event.currentTarget.style.opacity = '0.4';
-        const initialPosition = parseInt(event.currentTarget.dataset.position)//배열 인덱스
-        setDragAndDrop({
-            ...dragAndDrop,
-            draggedFrom: initialPosition,
-            originalOrder:list
-        }
-        )
     }
     // 드래그 하면서 아이템이 겹칠때
     const onDragOver = (event) => {
         console.log('아이템 들고있을때')
-        event.preventDefault(); //엘리먼트는 서로 위치가 변할수 없기 때문에 이벤트 동작하기전에 멈춰준다
-        let newList = dragAndDrop.originalOrder;
-        const draggedFrom = dragAndDrop.draggedFrom; // 드래그 되는 엘리먼트 인덱스
-        const draggedTo  = parseInt(event.currentTarget.dataset.position); //놓을 수 있는 영역의 인덱스(끝), 겹쳐있는 인덱스
-        const itemDragged = newList[draggedFrom]// 드래그 되는 엘리먼트
-        const remainingItems = newList.filter((item,index) => index !== draggedFrom) // 드래그하고있는 엘리먼트 빼고 배열목록
-        newList = [										// 드래그 시작, 끝 인덱스를 활용해 새로운 배열로 반환해줌
-            ...remainingItems.slice(0, draggedTo), //0부터 드래그 
-            itemDragged,                           //드래그 되는 엘리먼트
-            ...remainingItems.slice(draggedTo),     //겹치는 엘리먼트 이후의 배열 반환
-        ];
-        if(draggedTo !== dragAndDrop.draggedTo){ //인덱스 포지션이 다른 경우에만 set 해준다
-            setDragAndDrop ({
-                ...dragAndDrop,
-                updatedOrder:newList,
-                draggedTo: draggedTo
-            })
-        }
     }
     //드래그한 엘리먼트를 놓을때
     const onDrop = (event) => {
         console.log('드래그 영역 안에 아이템을 드랍');
-        setList(dragAndDrop.updatedOrder); //리스트를 재구성
-        setDragAndDrop({//상태 초기화
-          ...dragAndDrop,
-          draggedFrom: null,
-          draggedTo: null,
-        });
     };
     //드래그 범위를 벗어나면
     const onDragLeave = (event) => {
         console.log('드래그 영역을 벗어남');
-        event.currentTarget.classList.remove("over");
-        setDragAndDrop({
-          ...dragAndDrop,
-          draggedTo: null,
-        });
     };
     // 잡은 Item이 다른 Item이랑 겹쳤을 때 발생<겹쳐졌을 때>
   const onDragEnter = (event) => { 
     console.log('아이템이 다른것과 겹침');
-    event.currentTarget.classList.add("over");
   };
   // 잡은 Item을 놓았을 때 발생
   const onDragEnd = (event) => {
     console.log('아무영역에 드래그를 놓음');
-    event.currentTarget.style.opacity = "1";
-    let newList = dragAndDrop.originalOrder;
-    const draggedFrom = dragAndDrop.draggedFrom; // 드래그 되는 엘리먼트 인덱스
-    const remainingItems = newList.filter((item,index) => index !== draggedFrom) // 드래그하고있는 엘리먼트 빼고 배열목록
-    console.log(remainingItems);
-    const listItens = document.querySelectorAll(".draggable");
-    setDragAndDrop(pre =>({
-        ...pre,
-        updatedOrder:remainingItems
-    }
-    ))
-    console.log(dragAndDrop.updatedOrder)
-    setList(dragAndDrop.updatedOrder); //리스트를 재구성
-    listItens.forEach((item) => {
-      item.classList.remove("over");
-    });
   };
   const addEmployee = () => {
       const item = {
