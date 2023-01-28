fa8c149757ac823b880b0d4b61a90b0f555804d2
unknown <matebe12@gmail.com>
2023-01-28 17:14:56 +0900

    refactoring

--- src/drag.js
+++ src/drag.js
@@ -1,68 +1,66 @@
 import { useState } from 'react';
 import { uuid } from 'uuid/v4';
 import './App.css'
+import './style/common.css'
 import { useEffect } from 'react';
 const Drag = ()  => {
     const [list,setList] = useState([
         {
-            emoji: Math.random() * 100,
+            emoji: <span>👦</span>,
             name: '이상연'
         },
         {
-            emoji: Math.random() * 100,
-            name: '김이언'
+            emoji: <span>😇</span>,
+            name: '최인묵'
         },
         {
-            emoji: Math.random() * 100,
-            name: '유병학'
+            emoji: <span>😎</span>,
+            name: '권혁민'
         },
         {
-            emoji: Math.random() * 100,
-            name: '꼼짱모'
+            emoji: <span>👻</span>,
+            name: '정용우'
         },
         {
-            emoji: Math.random() * 100,
-            name: '코우자'
+            emoji: <span>😺</span>,
+            name: '고앙다'
         },
         {
-            emoji: Math.random() * 100,
-            name: '멀방맨'
-        },
-        {
-            emoji: Math.random() * 100,
-            name: '코린'
+            emoji: <span>👤</span>,
+            name: '송은석'
         },
     ])
     const [list2,setList2] = useState([
         {
-            emoji: Math.random() * 100,
+            emoji: <span>👦</span>,
             name: '이상연2'
         },
         {
-            emoji: Math.random() * 100,
+            emoji: <span>😇</span>,
             name: '김이언2'
         },
         {
-            emoji: Math.random() * 100,
+            emoji: <span>😎</span>,
             name: '유병학2'
         },
         {
-            emoji: Math.random() * 100,
+            emoji: <span>👻</span>,
             name: '꼼짱모2'
         },
         {
-            emoji: Math.random() * 100,
+            emoji: <span>😺</span>,
             name: '코우자2'
         },
         {
-            emoji: Math.random() * 100,
+            emoji: <span>👤</span>,
             name: '멀방맨2'
         },
         {
-            emoji: Math.random() * 100,
+            emoji: <span>👻</span>,
             name: '코린2'
         },
     ])
+    const [total,setTotal] = useState(list.length + list2.length);
     const [dragAndDrop, setDragAndDrop] = useState({
         draggedFrom: null, //드래그 시작 인덱스
         draggedTo: null, // 변경될 드래그 인덱스
@@ -83,9 +81,7 @@ const Drag = ()  => {
         console.log('드래그 시작')
         event.currentTarget.style.opacity = '0.4';
         const initialPosition = parseInt(event.currentTarget.dataset.position)//배열 인덱스
-        const initialListIndex = parseInt(event.currentTarget.dataset.list)
-        console.log(event.currentTarget.dataset.position);
-        console.log(event.currentTarget.dataset.list);
+        const initialListIndex = parseInt(event.currentTarget.dataset.list)// 리스트 목록 순번
         if(initialListIndex === 1 && initialPosition){
             setDragAndDrop({
                 ...dragAndDrop,
@@ -94,8 +90,8 @@ const Drag = ()  => {
             })
             setCurrentList(1)
         }
-        else{
-            console.log(initialPosition);
+        else {
+            // console.log(initialPosition);
             setDragAndDrop2({
                 ...dragAndDrop2,
                 draggedFrom: initialPosition,
@@ -104,15 +100,19 @@ const Drag = ()  => {
             setCurrentList(2)
         }
         }
-    useEffect(() => {
-        console.log(dragAndDrop2)
-    },[dragAndDrop2])
-    // 드래그 하면서 아이템 들고있을때
+    // 드래그 하면서 다른 아이템의 위로 들어올 때
+    const onDragEnter = (event) => { 
+        console.log('아이템이 다른것과 겹침');
+        event.currentTarget.classList.add("over");
+    };
+    useEffect(()=> {
+        console.log(currentList)
+    },[currentList])
+    // 잡은 Item이 다른 Item이랑 겹쳤을 때 발생<겹쳐졌을 때>
     const onDragOver = (event) => {
         console.log('아이템 들고있을때')
         event.preventDefault();
-        console.log(event.currentTarget.dataset.list)
-        const initialListIndex = parseInt(event.currentTarget.dataset.list) //ㅎ
+        const initialListIndex = parseInt(event.currentTarget.dataset.list) //현재 인덱스
         if(currentList === initialListIndex){ //같을때
             if(currentList === 1){//첫번째 리스트
                 let newList = dragAndDrop.originalOrder;
@@ -125,8 +125,7 @@ const Drag = ()  => {
                     itemDragged,
                     ...remainingItems.slice(draggedTo),
                 ];
-                console.log(dragAndDrop)
-                if(draggedTo !== dragAndDrop.draggedTo){
+                if(draggedTo !== dragAndDrop.draggedTo){ //같은 엘리먼트 일때
                     setDragAndDrop ({
                         ...dragAndDrop,
                         updatedOrder:newList,
@@ -144,7 +143,7 @@ const Drag = ()  => {
                     itemDragged,
                     ...remainingItems.slice(draggedTo),
                 ];
-                console.log(dragAndDrop2)
+                // console.log(dragAndDrop2)
                 if(draggedTo !== dragAndDrop2.draggedTo){
                     setDragAndDrop2 ({
                         ...dragAndDrop2,
@@ -161,10 +160,6 @@ const Drag = ()  => {
                 const draggedTo  = parseInt(event.currentTarget.dataset.position); //놓을 수 있는 영역의 인덱스(끝)
                 // const itemDragged = newList[draggedFrom]// 드래그 겹치는 엘리먼트
                 const remainingItems = list2.filter((item,index) => index !== draggedFrom) // 드래그하고있는 엘리먼트 빼고 배열목록
-                console.log(newList)
-                console.log(...newList.slice(0, draggedTo))
-                console.log(dragAndDrop2.updatedOrder[draggedFrom])
-                console.log(...newList.slice(draggedTo))
                 
                 newList = [										// 드래그 시작, 끝 인덱스를 활용해 새로운 배열로 반환해줌
                     ...newList.slice(0, draggedTo),
@@ -172,8 +167,6 @@ const Drag = ()  => {
                     list2[draggedFrom],
                     ...newList.slice(draggedTo),
                 ];
-                console.log(newList)
-                console.log(remainingItems)
                 if(draggedTo){
                     setDragAndDrop ({
                         ...dragAndDrop,
@@ -192,10 +185,6 @@ const Drag = ()  => {
                 const draggedTo  = parseInt(event.currentTarget.dataset.position); //놓을 수 있는 영역의 인덱스(끝)
                 // const itemDragged = newList[draggedFrom]// 드래그 겹치는 엘리먼트
                 const remainingItems = list.filter((item,index) => index !== draggedFrom) // 드래그하고있는 엘리먼트 빼고 배열목록
-                console.log(newList)
-                console.log(...newList.slice(0, draggedTo))
-                console.log(dragAndDrop.updatedOrder[draggedFrom])
-                console.log(...newList.slice(draggedTo))
                 
                 newList = [										// 드래그 시작, 끝 인덱스를 활용해 새로운 배열로 반환해줌
                     ...newList.slice(0, draggedTo),
@@ -203,8 +192,6 @@ const Drag = ()  => {
                     list[draggedFrom],
                     ...newList.slice(draggedTo),
                 ];
-                console.log(newList)
-                console.log(remainingItems)
                 if(draggedTo){
                     setDragAndDrop ({
                         ...dragAndDrop,
@@ -221,9 +208,19 @@ const Drag = ()  => {
         }
         
     }
-    useEffect(()=>{
-        console.log(dragAndDrop)
-    },[dragAndDrop])
+    //드래그 범위를 벗어나면
+    const onDragLeave = (event) => {
+        console.log('드래그 영역을 벗어남');
+        event.currentTarget.classList.remove("over");
+        setDragAndDrop({
+            ...dragAndDrop,
+            draggedTo: null,
+        });
+        setDragAndDrop2({
+            ...dragAndDrop2,
+            draggedTo: null,
+        });
+    };
     //드래그한 엘리먼트를 놓을때
     const onDrop = (event) => {
         console.log('드래그 영역 안에 아이템을 놓음');
@@ -247,25 +244,8 @@ const Drag = ()  => {
         //   draggedTo: null,
         // });
     };
-    //드래그 범위를 벗어나면
-    const onDragLeave = (event) => {
-        console.log('드래그 영역을 벗어남');
-        event.currentTarget.classList.remove("over");
-        setDragAndDrop({
-          ...dragAndDrop,
-          draggedTo: null,
-        });
-        setDragAndDrop2({
-            ...dragAndDrop2,
-            draggedTo: null,
-          });
-    };
-    // 잡은 Item이 다른 Item이랑 겹쳤을 때 발생<겹쳐졌을 때>
-  const onDragEnter = (event) => { 
-    console.log('아이템이 다른것과 겹침');
-    event.currentTarget.classList.add("over");
-  };
-  // 잡은 Item을 놓았을 때 발생
+    
+    // 잡은 Item을 놓았을 때 발생
   const onDragEnd = (event) => {
     console.log('아무영역에 드래그를 놓음');
     event.currentTarget.style.opacity = "1";
@@ -273,41 +253,40 @@ const Drag = ()  => {
             let newList = dragAndDrop.originalOrder;
             const draggedFrom = dragAndDrop.draggedFrom; // 드래그 되는 엘리먼트 인덱스
             const remainingItems = newList.filter((item,index) => index !== draggedFrom) // 드래그하고있는 엘리먼트 빼고 배열목록
-            console.log(remainingItems);
             const listItens = document.querySelectorAll(".draggable");
             setDragAndDrop(pre =>({
                 ...pre,
                 updatedOrder:remainingItems
             }
             ))
-            console.log(dragAndDrop.updatedOrder)
-            setList(dragAndDrop.updatedOrder); //리스트를 재구성
+            // console.log(dragAndDrop.updatedOrder)
+                setList(dragAndDrop.draggedTo && event.target.parentElement ? newList : dragAndDrop.updatedOrder); //리스트를 재구성
             listItens.forEach((item) => {
             item.classList.remove("over");
             });
-        }else{
+        } else if (currentList === 2){
             let newList = dragAndDrop2.originalOrder;
             const draggedFrom = dragAndDrop2.draggedFrom; // 드래그 되는 엘리먼트 인덱스
             const remainingItems = newList.filter((item,index) => index !== draggedFrom) // 드래그하고있는 엘리먼트 빼고 배열목록
-            console.log(remainingItems);
-            const listItens = document.querySelectorAll(".draggable");
+            // console.log(remainingItems);
+            const listItems = document.querySelectorAll(".draggable");
             setDragAndDrop2(pre =>({
                 ...pre,
                 updatedOrder:remainingItems
             }
             ))
-            console.log(dragAndDrop2.updatedOrder)
-            setList2(dragAndDrop2.updatedOrder); //리스트를 재구성
-            listItens.forEach((item) => {
+            setList2(dragAndDrop2.draggedTo && event.target.parentElement ? newList : dragAndDrop2.updatedOrder); //리스트를 재구성
+            listItems.forEach((item) => {
             item.classList.remove("over");
             });
     }
   };
-//   useEffect(()=>{
-//     setList(dragAndDrop.updatedOrder)
-//   },[dragAndDrop.updatedOrder])
+  useEffect(()=>{
+    setTotal(list.length + list2.length)
+  },[dragAndDrop])
     return (
         <div style={{display:'flex',}}>
+            {total}
             <ul>
                 {list.map((item, index) => {
                     return (
